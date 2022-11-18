231cacaded9f563cd6e4843b6a6183d53652bb26
yagsim <yagosimofernandez@gmail.com>
2022-11-18 13:23:34 +0100

    creación DB con palabras y substitución

--- html/wp-content/plugins/malsonantes/malsonantes.php
+++ html/wp-content/plugins/malsonantes/malsonantes.php
@@ -7,24 +7,72 @@ Author: Yago Simoes
 Version: 1.0
 Author URI: http://ya.go/
 */
-//Reemplaza palabras
 
-/*function palabrasMalsonantes( $content ) {
-    $vulgar_words = array( $vulgar_word1, $vulgar_word2, $vulgar_word3, $vulgar_word4 );
+function crearDB() {
+    global $wpdb;
 
-    foreach ( $vulgar_words as $word ) {
-        $hashed_word = substr( $word, 0, 1 ) . str_repeat( '*', strlen( $word ) - 1 );
-        $content = str_replace( $word, $hashed_word, $content );
-    }
+    $charset_collate = $wpdb->get_charset_collate();
 
-    return $content;
+// le añado el prefijo a la tabla
+    $table_name1 = $wpdb->prefix . 'palabrasMalsonantes';
+    $table_name2 = $wpdb->prefix . 'palabrasBonitas';
+
+// creamos la sentencia sql
+
+    $sql = "CREATE TABLE $table_name1 (
+id mediumint(9) NOT NULL AUTO_INCREMENT,
+text text NOT NULL,
+PRIMARY KEY (id)
+) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta($sql);
+
+    $sql2 = "CREATE TABLE $table_name2 (
+id mediumint(9) NOT NULL AUTO_INCREMENT,
+text text NOT NULL,
+PRIMARY KEY (id)
+) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta($sql2);
 }
 
-*/
+add_action('plugins_loaded', 'crearDB');
+function insertarMalsonantes(){
+    global $wpdb;
+    $table_name1=$wpdb->prefix .'palabrasMalsonantes';
+    $sql1 = "INSERT INTO $table_name1 (text) VALUES (1,'mierda')";
+    $sql2 = "INSERT INTO $table_name1(text) VALUES (2,'cabrón')";
+    $sql3 = "INSERT INTO $table_name1 (text) VALUES (3,'mamón')";
+
+    global $wpdb;
+    $table_name2=$wpdb->prefix.'palabrasBonitas';
+    $sql4 = "INSERT INTO $table_name2 (text) VALUES (1,'popo')";
+    $sql5 = "INSERT INTO $table_name2(text) VALUES (2,'malo')";
+    $sql6 = "INSERT INTO $table_name2 (text) VALUES (3,'malisimo')";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta($sql1);
+    dbDelta($sql2);
+    dbDelta($sql3);
+    dbDelta( $sql4 );
+    dbDelta( $sql5 );
+    dbDelta( $sql6 );
+}
+ add_action('plugins_loaded', 'insertarMalsonantes');
+
 function cambiarMalsonantes( $text ){
-        $malsonantes=array("mierda","cabrón","mamón");
-        $correctas=array("popo","malo","malo");
+    global $wpdb;
+    $table_name1 = $wpdb->prefix . 'palabrasMalsonantes';
+    $table_name2= $wpdb->prefix . 'palabrasBonitas';
+
+
+       /* $malsonantes=array("mierda","cabrón","mamón");
+        $correctas=array("popo","malo","malo");*/
+    $malsonantes=dbDelta("SELECT text FROM $table_name1",OBJECT);
+    $correctas=dbDelta("SELECT text FROM $table_name2",OBJECT);
     return str_replace($malsonantes, $correctas, $text);
 }
 
-add_filter( 'the_content', 'cambiarMalsonantes' );
+ add_filter( 'the_content', 'cambiarMalsonantes' );
